843961e781f6ae54af1a57377c111472149143da
Ronny Reichmann <ronny.reichmann@icloud.com>
2018-01-05 17:46:24 +0100

    Extraction from teth

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # auid
 Acceptably Unique Identifier
+
+Create unique IDs for storage or identification purposes:
+
+```
+import auid from 'auid';
+// or: const auid = require('auid');
+const key = auid();
+```
+
+License: MIT
---
+++ auid.js
@@ -0,0 +1,18 @@
+/* Copyright 2017 Ronny Reichmann */
+/* AUID Acceptably Unique Identifier */
+
+const randomBytes = require('crypto').randomBytes
+let currentCount = 0
+
+function auid () {
+  const zoneComp = Number(730 + (new Date()).getTimezoneOffset()).toString(16)
+  const stampComp = Number(Date.now()).toString(16)
+  currentCount = currentCount === 11111111
+    ? 0
+    : currentCount + 1
+  const countComp = Number(currentCount).toString(16)
+  const randomComp = randomBytes(2).toString('hex')
+  return `g${zoneComp}h${stampComp}i${countComp}j${randomComp}`
+}
+
+module.exports = auid
